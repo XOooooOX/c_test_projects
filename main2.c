0b61a4de36496f2ebc0d7ad1b896ea24d4c4b4a2
M.Ghelichkhani <m.ghelichkhani@ebidar.net>
2025-05-12 12:59:01 +0330

    update

--- main2.c
+++ main2.c
@@ -13,10 +13,42 @@ typedef struct {
     int age;
 } student;
 
+typedef struct {
+    int id;
+    char name[10];
+    int age;
+} man;
+
+
+// ijad yek method va seda zadan an ba pointer mesl delagate
+int add(int a,int b) { return  a + b;}
+int subtract(int a,int b) { return  a - b;}
 
 int main(){
+
+    int (*func_ptr)(int,int);
+
+    func_ptr = add;
+
+    printf("jam %d\n",func_ptr(5,6));
+
+    func_ptr = subtract;
+
+    printf("tafrigh %d\n",func_ptr(8,4));
+
+
+    man m;
+    strcpy(m.name,"hosein");
+    m.id = 90;
+    m.age = 90;
+
+
     student s1;
     s1.age = 1;
+    // intory khely behine tare , vaghti tol arraye ro moshakas mikonim mesl model man
+    // agar hata yek char meghdar begire baghi khone ha ham khali mimonan
+    // vall to model char *name vaghti ba malloc behesh meghdar midim aval be tedead ma faza mide
+    // vali bad az meghdar dehi ba string size dorost mishe
     s1.name = malloc(100 * sizeof (char));
     if(s1.name == NULL){
         perror("malloc");
@@ -27,6 +59,15 @@ int main(){
 
     student s2;
     student *ptrstudent = &s2;
+
+    // mesl generic ya object
+    void  *ptrtostudentbyvoid = &s2;
+
+    ((student*)ptrtostudentbyvoid)->id = 44;
+
+    student **ptrtoptrstudent = &ptrstudent;
+
+    (*ptrtoptrstudent)->id = 3;
 
     ptrstudent->id =2;
     ptrstudent->age =90;
